7e523e474899b0b9d48ac1810d4a6c48ec4e2d1d
GIgibit <l.chougrad@gmail.com>
2026-02-19 23:12:48 +0100

    fixing django run issues

--- trading_bot/settings.py
+++ trading_bot/settings.py
@@ -12,25 +12,45 @@ allowed_hosts_env = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost")
 ALLOWED_HOSTS = [host.strip() for host in allowed_hosts_env.split(",")]
 
 INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
     "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
     "django.contrib.staticfiles",
-    "trading_bot.bot",
+    "trading_bot.bot",  
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 ROOT_URLCONF = "trading_bot.urls"
-TEMPLATES = [{
-    "BACKEND": "django.template.backends.django.DjangoTemplates",
-    "DIRS": [BASE_DIR / "templates"],
-    "APP_DIRS": True,
-    "OPTIONS": {
-        "context_processors": [],
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],  # optional
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",   # fixes W411
+                "django.contrib.auth.context_processors.auth",  # fixes E402
+                "django.contrib.messages.context_processors.messages",  # fixes E404
+            ],
+        },
     },
-}]
+]
 WSGI_APPLICATION = "trading_bot.wsgi.application"
+
+SESSION_ENGINE = "django.contrib.sessions.backends.db"
 
 DATABASES = {
     "default": {
